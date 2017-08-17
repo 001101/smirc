a1219d7940a7724264a19fcc088e6536273dbfab
Sean Enck <enckse@gmail.com>
2017-08-17 14:54:28 -0400

    towards #8

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usb/bin/python
+
+"""Setup for smirc."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='smirc',
+    version="0.4.0",
+    description='system monitor IRC bot',
+    url='https://github.com/epiphyte/smirc',
+    license='MIT',
+    packages=['smirc'],
+    install_requires=['pyzmq', 'irc', 'systemd'],
+    entry_points={
+        'console_scripts': [
+            'smirc = smirc.smirc:main',
+        ],
+    },
+)
